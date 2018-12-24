c16321f9592193f40ece4ed35f9c047036f36e4c
Marcel Otón Pàmies <marotpam@gmail.com>
2018-12-25 00:00:25 +0100

    Add solutions for day 12

---
+++ 12/12.1/main.go
@@ -0,0 +1,223 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+)
+
+type pot struct {
+	index          int
+	hasPlant       bool
+	willHavePlant  bool
+	previous, next *pot
+}
+
+func (p *pot) willGrow() bool {
+	return !p.hasPlant && p.willHavePlant
+}
+
+func (p *pot) willDie() bool {
+	return p.hasPlant && !p.willHavePlant
+}
+
+func (p *pot) getRule() string {
+	buf := bytes.Buffer{}
+
+	for po := p.previous.previous; po != p.next.next.next; po = po.next {
+		r := '.'
+		if po.hasPlant {
+			r = '#'
+		}
+		buf.WriteRune(r)
+	}
+
+	return buf.String()
+}
+
+type garden struct {
+	first, last *pot
+	rules       map[string]string
+}
+
+func newGarden(initialState string, rules map[string]string) *garden {
+	minusThree := &pot{
+		index: -3,
+	}
+	minusTwo := &pot{
+		index:    -2,
+		previous: minusThree,
+	}
+	minusThree.next = minusTwo
+
+	minusOne := &pot{
+		index:    -1,
+		previous: minusTwo,
+	}
+	minusTwo.next = minusOne
+
+	var previous *pot
+
+	for i := range initialState {
+		pot := &pot{
+			index:    i,
+			hasPlant: initialState[i] == '#',
+		}
+		if previous == nil {
+			pot.previous = minusOne
+			minusOne.next = pot
+		} else {
+			pot.previous = previous
+			previous.next = pot
+		}
+		previous = pot
+	}
+
+	plusOne := &pot{
+		index:    len(initialState),
+		previous: previous,
+	}
+	previous.next = plusOne
+
+	plusTwo := &pot{
+		index:    plusOne.index + 1,
+		previous: plusOne,
+	}
+	plusOne.next = plusTwo
+
+	plusThree := &pot{
+		index:    plusTwo.index + 1,
+		previous: plusTwo,
+	}
+	plusTwo.next = plusThree
+
+	return &garden{minusOne.next, plusOne, rules}
+}
+
+func (g *garden) growGeneration() {
+	first := g.first
+
+	previous := g.first.previous
+	g.grow(previous)
+	if previous.willGrow() {
+		newHead := &pot{
+			index: previous.previous.index-1,
+			next:  previous.previous,
+		}
+		previous.previous.previous = newHead
+		g.first = previous
+	} else if previous.willDie() {
+		newNext := g.first.next
+		newNext.previous = nil
+		g.first = newNext
+	}
+
+	var p *pot
+	for p = first; p != g.last; p = p.next {
+		g.grow(p)
+	}
+
+	afterLast := p
+	g.grow(afterLast)
+
+	if afterLast.willGrow() {
+		g.last = afterLast.next
+		newTail := &pot{
+			index:    afterLast.next.next.index+1,
+			previous: afterLast.next.next,
+		}
+		afterLast.next.next.next = newTail
+	} else if afterLast.willDie() {
+		g.last = afterLast.previous
+	}
+
+	for p := g.first; p != g.last; p = p.next {
+		p.hasPlant = p.willHavePlant
+	}
+}
+
+func (g *garden) grow(p *pot) {
+	buf := bytes.Buffer{}
+
+	for po := p.previous.previous; po != p.next.next.next; po = po.next {
+		if po == nil {
+			return
+		}
+		r := '.'
+		if po.hasPlant {
+			r = '#'
+		}
+		buf.WriteRune(r)
+	}
+
+	applicableRule := buf.String()
+
+	if r, ok := g.rules[applicableRule]; ok {
+		if r == "#" {
+			p.willHavePlant = true
+		} else {
+			p.willHavePlant = false
+		}
+	} else {
+		p.willHavePlant = false
+	}
+}
+
+func (g *garden) potsAsString() string {
+	var buf bytes.Buffer
+	i := 0
+	for p := g.first; p != g.last; p = p.next {
+		r := '.'
+		if p.hasPlant {
+			r = '#'
+		}
+		buf.WriteRune(r)
+		i++
+	}
+
+	return buf.String()
+}
+
+func (g *garden) growFor(nGenerations int) {
+	for i := 0; i < nGenerations; i++ {
+		g.growGeneration()
+	}
+}
+
+func (g *garden) sumPotIndexesWithPlants() int {
+	i := 0
+
+	for p := g.first; p != g.last; p = p.next {
+		if p.hasPlant {
+			i += p.index
+		}
+	}
+
+	return i
+}
+
+func main() {
+	fh, _ := os.Open("input.txt")
+	defer fh.Close()
+
+	fs := bufio.NewScanner(fh)
+	fs.Scan()
+	var initialState string
+	fmt.Sscanf(fs.Text(), `initial state: %s`, &initialState)
+
+	fs.Scan()
+
+	rules := map[string]string{}
+	for fs.Scan() {
+		var rule, result string
+		fmt.Sscanf(fs.Text(), `%s => %s`, &rule, &result)
+		rules[rule] = result
+	}
+
+	g := newGarden(initialState, rules)
+
+	g.growFor(20)
+
+	fmt.Println(g.sumPotIndexesWithPlants()) // 3903
+}
---
+++ 12/12.2/main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestItCanDetectARuleThatAppliesToThePotInTheMiddle(t *testing.T) {
+	rules := map[string]string{}
+	rules[".#..."] = "#"
+
+	g := newGarden(".#...", rules)
+	g.growGeneration()
+
+	if p := g.potsAsString(); p != ".##.." {
+		t.Errorf("Expecting pots to become .##.. after one generation, got %s\n", p)
+	}
+}
+
+func TestItCanDetectARuleThatAppliesToThePotBeforeTheFirstOne(t *testing.T) {
+	rules := map[string]string{}
+	rules["...#."] = "#"
+
+	g := newGarden("#....", rules)
+	g.growGeneration()
+
+	expected := "##...."
+
+	if p := g.potsAsString(); p != expected {
+		t.Errorf("Expecting pots to become %s after one generation, got %s\n", expected, p)
+	}
+}
+
+func TestItCanDetectARuleThatAppliesToThePotAfterTheLastOne(t *testing.T) {
+	rules := map[string]string{}
+	rules["#...."] = "#"
+
+	g := newGarden("...#.", rules)
+	g.growGeneration()
+	expected := "...#.#"
+
+	if p := g.potsAsString(); p != expected {
+		t.Errorf("Expecting pots to become %s after one generation, got %s\n", expected, p)
+	}
+}
