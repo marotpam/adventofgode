7d9c8f68357f8c2338894508e7abaaee74268b58
Marcel Otón Pàmies <marotpam@gmail.com>
2019-12-07 15:48:49 +0100

    Refactor solution for 7.2 introducing amplifier struct

--- 2019/7/7.2.go
+++ 2019/7/7.2.go
@@ -13,43 +13,76 @@ func CalculateSecondOptimalThrusterSequence(inputs []int) int {
 	return max
 }
 
-type readWriter struct {
+type io struct {
 	elements []int
 }
 
-func (r *readWriter) write(n int) {
-	r.elements = append(r.elements, n)
+func (i *io) write(n int) {
+	i.elements = append(i.elements, n)
 }
 
-func (r *readWriter) read() int {
-	a, rest := r.elements[0], r.elements[1:]
-	r.elements = rest
+func (i *io) read() int {
+	a, rest := i.elements[0], i.elements[1:]
+	i.elements = rest
 
 	return a
 }
 
-func calculateSecondOptimalThrusterSequenceWithInputs(instructions []int, sequence []int) int {
-	readWriters :=make([]*readWriter, len(sequence))
-	positions := make(map[int]int, len(sequence))
-	ampInstructions := make(map[int][]int, len(sequence))
-	for i, s := range sequence {
-		readWriters[i] = &readWriter{elements: []int{s}}
-		positions[i] = 0
-		ampInstructions[i] = append([]int{}, instructions...)
-	}
-	readWriters[0].elements = append(readWriters[0].elements, 0)
+type amplifier struct {
+	*io
+	currentPosition int
+	instructions    []int
+}
 
-	for i := 0; ;  {
-		pos := CalculateSecondOpcode(ampInstructions[i], readWriters[i], readWriters[(i+1)%len(sequence)], positions[i])
-		positions[i] = pos
-		if pos == -1 {
-			delete(positions, i)
-			if len(positions) == 0 {
-				return readWriters[0].elements[len(readWriters[0].elements)-1]
-			}
+func newAmplifier(phaseSetting int, instructions []int) *amplifier {
+	return &amplifier{
+		instructions:    instructions,
+		io:              &io{elements: []int{phaseSetting}},
+		currentPosition: 0,
+	}
+}
+
+func (a *amplifier) isDone() bool {
+	return a.currentPosition == -1
+}
+
+func (a *amplifier) input(i int) {
+	a.io.elements = append(a.io.elements, i)
+}
+
+func (a *amplifier) output() int {
+	return a.io.elements[len(a.io.elements)-1]
+}
+
+func calculateSecondOptimalThrusterSequenceWithInputs(instructions []int, sequence []int) int {
+	amplifiers := make([]*amplifier, len(sequence))
+	for i, s := range sequence {
+		amplifiers[i] = newAmplifier(s, append([]int{}, instructions...))
+	}
+	amplifiers[0].input(0)
+
+	for i := 0; ; i = (i + 1) % len(sequence) {
+		amp := amplifiers[i]
+		amp.currentPosition = CalculateSecondOpcode(
+			amp.instructions,
+			amp,
+			amplifiers[(i+1)%len(sequence)],
+			amp.currentPosition,
+		)
+
+		if allDone(amplifiers) {
+			return amplifiers[0].output()
 		}
-		i = (i+1)%len(sequence)
 	}
 
 	return 0
+}
+
+func allDone(as []*amplifier) bool {
+	for _, a := range as {
+		if !a.isDone() {
+			return false
+		}
+	}
+	return true
 }
