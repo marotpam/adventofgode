8425b6a33db27d1c2ca15ee60a3796c6444d24db
Marcel Otón Pàmies <marotpam@gmail.com>
2019-12-08 12:46:43 +0100

    Add solution for 8.1

---
+++ 2019/8/8.1.go
@@ -0,0 +1,31 @@
+package main
+
+import "math"
+
+type layer struct {
+	digits map[rune]int
+}
+
+func newLayer() *layer {
+	return &layer{digits: make(map[rune]int, 10)}
+}
+
+func CalculateFirstMultiplication(s string, width, height int) int {
+	minCountOfZeros := math.MaxInt64
+	layerSize := width * height
+
+	l := newLayer()
+	result := 0
+	for i, r := range s {
+		if i%layerSize == 0 {
+			zerosInLayer := l.digits['0']
+			if zerosInLayer > 0 && zerosInLayer < minCountOfZeros {
+				result = l.digits['1']*l.digits['2']
+				minCountOfZeros = zerosInLayer
+			}
+			l = newLayer()
+		}
+		l.digits[r] = l.digits[r] + 1
+	}
+	return result
+}
