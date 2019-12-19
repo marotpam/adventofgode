b4f7e8d15c5428f389b12e27c2ab31eb136096cc
Marcel Otón Pàmies <marotpam@gmail.com>
2019-12-19 18:06:50 +0100

    Add solution for day 19.1

---
+++ 2019/19/19.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/marotpam/adventofgode/2019/intcode"
+)
+
+const (
+	outputPulled = 1
+)
+
+type position struct {
+	x, y int
+}
+
+type drone struct {
+	interpreter     *intcode.Interpreter
+	maxPosition     position
+	currentPosition position
+	timesPulled     int
+	readsCounter    int
+}
+
+func (d *drone) Read() int {
+	var position int
+	if d.readsCounter%2 == 0 {
+		position = d.currentPosition.x
+	} else {
+		position = d.currentPosition.y
+	}
+	d.readsCounter++
+
+	return position
+}
+
+func (d *drone) Write(n int) {
+	if n == outputPulled {
+		d.timesPulled++
+	}
+}
+
+func newDrone(maxPosition position) *drone {
+	return &drone{
+		interpreter: intcode.NewInterpreter(),
+		maxPosition: maxPosition,
+		currentPosition: position{
+			x: 0,
+			y: 0,
+		},
+		timesPulled: 0,
+	}
+}
+
+func (d *drone) work(instructions []int) {
+	for d.currentPosition.y < d.maxPosition.y {
+		d.interpreter.Run(instructions, d, d)
+
+		d.currentPosition.x += 1
+		if d.currentPosition.x == d.maxPosition.x {
+			d.currentPosition.x = 0
+			d.currentPosition.y++
+		}
+	}
+}
+
+func CountAffectedPoints(instructions []int) int {
+	drone := newDrone(position{
+		x: 50,
+		y: 50,
+	})
+	drone.work(instructions)
+
+	return drone.timesPulled
+}
